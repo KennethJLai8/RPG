7092a8ec58137e1ddaaadaf81f89342b694d5592
jchan396 <jchan396@ucr.edu>
2021-02-24 11:25:59 -0800

    initial character header

---
+++ character.hpp
@@ -0,0 +1,32 @@
+#ifndef CHARACTER_HPP
+#define CHARACTER_HPP
+#include <string>
+
+using namespace std;
+
+class Character {
+private:
+string name;
+int hp;
+int armor;
+int attack;
+int crowd_control;
+
+public:
+
+Character();
+
+int setHp();
+int setArmor();
+int setAttack();
+int crowd_control();
+
+
+int getHp();
+int getArmor();
+int getAttack;
+int getCrowdControl;
+
+};
+
+#endif
