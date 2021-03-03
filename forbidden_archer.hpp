3488f49703a4ee9aa0876cd70a5eeb5d175f93d0
Kenneth Lai <kennethlai@Kenneths-MacBook-Pro-2.local>
2021-03-02 23:17:52 -0800

    rough draft

---
+++ forbidden_archer.hpp
@@ -0,0 +1,12 @@
+#ifndef __FORBIDDEN_ARCHER_HPP__
+#define __FORBIDDEN_ARCHER_HPP__
+#include "Character.hpp"
+
+class ForbiddenArcher : public Character
+{
+    this->name = "Arthur";
+    this->hp = 5;//hard
+    this->armor = "Brigandine";
+    this->attack = "Piercing Arrow";
+};
+#endif
