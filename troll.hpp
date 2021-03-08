51af7a66b1c7894c27e6fa5515b36707b71da290
jchan396 <77028709+jchan396@users.noreply.github.com>
2021-03-07 20:44:27 -0800

    troll derived class

---
+++ troll.hpp
@@ -0,0 +1,43 @@
+#ifndef __TROLL_HPP__
+#define __TROLL_HPP__
+#include "character.hpp"
+#include <string>
+using namespace std;
+
+class GenericTroll : public Character
+{
+    string name = "troll";
+    int hp = 10;
+    string armor = "forbidden cloak";
+    string block = "Club";
+    string attack = "rickroll";
+    string cc = "trollstice";
+
+    string get_name()
+    {
+      return name;
+    }
+
+    int get_hp()
+    {
+      return hp;
+    }
+
+    string get_block()
+    {
+      return block;
+    }
+
+    string get_attack()
+    {
+      return attack;
+    }
+
+    string get_cc()
+    {
+      return cc;
+    }
+
+};
+
+#endif //__GENERICTROLL_HPP__
